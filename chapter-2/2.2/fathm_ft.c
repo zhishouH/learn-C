aa6e2e101423cf9b12bafd3d932e20617e133bd5
zhishouhuang <zhishouhuang@gmail.com>
2019-07-18 20:12:56 +0800

    进一步使用C

--- chapter-2/2.2/fathm_ft.c
+++ chapter-2/2.2/fathm_ft.c
@@ -1,13 +1,12 @@
-//fathm_ft.c--°Ñ2Ó¢Ñ°×ª»»³ÉÓ¢³ß 
-	#include <stdio.h>
-	int main(void)
-	{
-		int feet, fathoms;
+#include <stdio.h>
+int main(void)
+{
+	int feet, fathoms;
 		
-		fathoms = 2;
-		feet = 6 * fathoms;
-		printf("There are %d feet in %d fathoms!\n", feet, fathoms);
-		printf("Yes, I said %d feet!\n", 6 * fathoms);
+	fathoms = 2;
+	feet = 6 * fathoms;
+	printf("There are %d feet in %d fathoms!\n", feet, fathoms);
+	printf("Yes, I said %d feet!\n", 6 * fathoms);
 		
-		return 0;
-	}
+	return 0;
+}
