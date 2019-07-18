5d2f62dfce82d592d598a991d4002a54cf861872
zhishouhuang <zhishouhuang@gmail.com>
2019-07-18 14:57:54 +0800

    简单的C程序

--- chapter-2/2.1/first.c
+++ chapter-2/2.1/first.c
@@ -4,7 +4,7 @@ int main(void)
 	int num;
 	num = 1;
 	
-	printf("I am a simple");
+	printf("I am a simple ");
 	printf("computer.\n");
 	printf("My favorite number is %d because it is first.\n",num);
 	
