17ca46edcd1ab17dd451786e423a18df44c20403
zhishouhuang <zhishouhuang@gmail.com>
2019-11-25 19:11:35 +0800

    注意花括号的使用

---
+++ chapter-6/6.3/while1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+	int n = 0;
+	
+	while (n < 3)
+		printf("n is %d\n",n);
+		n++;
+	printf("That`s all this program does\n");
+	
+	return 0;
+}
+//糟糕的代码创建了一个无限循环 
